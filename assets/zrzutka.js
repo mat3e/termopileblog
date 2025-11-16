8dc6420f2774c95901a7890ddbd3d16dc4a86fb3
mat3e <mat3e@interia.pl>
2025-11-16 22:05:54 +0100

    Add buy popup

--- assets/zrzutka.js
+++ assets/zrzutka.js
@@ -1,17 +1,30 @@
-if (sessionStorage.getItem('zrzutka already skipped') !== 'true') {
+setTimeout(function () {
+    const img = new Image();
+    img.src = 'https://polskietermopile.pl/projekty/400/400.png';
+}, 3_000);
+if (sessionStorage.getItem('buy already skipped') !== 'true' && !location.pathname.includes('/projekty/400/')) {
     setTimeout(function () {
         const z = document.createElement('dialog');
         z.classList.add('eUp');
-        z.id = 'zrzutka-dialog';
+        z.id = 'buy-dialog';
         z.style.width = '100%';
         z.style.maxWidth = '400px';
-        z.style.boxSizing = 'border-box';
         z.innerHTML = `<form method="dialog">
                 <button class="e-delete"><i aria-hidden="true">&times;</i></button>
             </form>
-            <div style="position: relative; width: 100%; max-width: 400px; height: 450px; overflow: hidden;"><iframe style="position: absolute; top:0; left: 0; bottom: 0; right: 0; width: 100%; height: 100%;" src="https://zrzutka.pl/8csrtk/widget/13" frameborder="0" scrolling="no"></iframe></div>`;
+            <div class="e-card white" style="max-width: 90%;margin: 0 auto">
+                <h2 style="text-align: center;margin-bottom: 0;margin-top:.5em;color: black">KOMIKS</h2>
+                <img src="https://polskietermopile.pl/projekty/400/400.png" alt="Logo 400"/>
+                <div class="card-body" style="text-align: center">
+                    <a class="e-btn success" href="https://polskietermopile.pl/projekty/400/kup/index.html">Kup teraz</a>
+                    <a style="margin-left: 0;color:black !important;border-color: black"
+                       class="e-btn outlined transparent" href="https://polskietermopile.pl/projekty/400/index.html">
+                        Zobacz więcej
+                    </a>
+                </div>
+            </div>`;
         document.body.appendChild(z);
         z.showModal();
-        sessionStorage.setItem('zrzutka already skipped', 'true');
-    }, 15_000);
+        sessionStorage.setItem('buy already skipped', 'true');
+    }, 5_000);
 }
